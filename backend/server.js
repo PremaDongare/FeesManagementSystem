93dc3aed705464ab5c86e6bc7fdb80fc341330e6
PremaDongare <premadongare17@gmail.com>
2025-07-17 23:33:48 +0530

    structure for render v5

--- backend/server.js
+++ backend/server.js
@@ -32,8 +32,12 @@ app.use('/api/students', studentsRoutes);
 app.use('/api/profile', profileRoutes);
 
 // Serve frontend (React build) for production
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+// });
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
