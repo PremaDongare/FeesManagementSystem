a12b9fec97273297be65fa2555d019965a855c27
PremaDongare <premadongare17@gmail.com>
2025-07-17 23:14:11 +0530

    structure for render v3

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use('/api/profile', profileRoutes);
 // Serve frontend (React build) for production
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
